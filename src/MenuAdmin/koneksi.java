c0ddf47542263d9a1cd5822d9b2d498e4824492f
Muchammad Alif Zaidan <zaidanalif22710@gmail.com>
2022-01-31 18:33:49 +0700

    first commit

---
+++ src/MenuAdmin/koneksi.java
@@ -0,0 +1,27 @@
+package MenuAdmin;
+import com.mysql.jdbc.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+
+
+public class koneksi {
+    public static Connection con;
+    
+    public static Connection koneksiDB() throws SQLException{
+        if (con == null) {
+            try {
+            String url = "jdbc:mysql://localhost/dbanjim";
+            String user = "root";
+            String pass = "";
+            DriverManager.registerDriver(new com.mysql.jdbc.Driver());
+            con = (Connection) DriverManager.getConnection(url,user,pass); 
+        } catch (Exception e) {
+             JOptionPane.showMessageDialog(null,"gagal koneksi "+e.getMessage());
+        }}
+        return con;
+    }
+      public static void main (String [ ] args)throws SQLException{
+        koneksiDB();
+      }}
+
